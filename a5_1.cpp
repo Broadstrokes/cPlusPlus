6ecfd15e72c0db4e489b1be969baf53bbf0082d1
Dan Sajjad <dan.sajjad@gmail.com>
2018-06-25 22:44:36 -0700

    Finish assignment a5_1

--- a5_1.cpp
+++ a5_1.cpp
@@ -1,13 +1,24 @@
 //
-//  a5_1.cpp
+//  main.cpp
+//  a5_1
 //
 //  Created by Dan Sajjad on 6/24/18.
 //  Copyright © 2018 Dan Sajjad. All rights reserved.
-//  Class: CS110B - Programming Fundamentals C++
+//
 
 /*
- 
+    The program records high-score data for some game. The program asks the user to enter five names, and five scores. It stores the data in memory, and prints it back out sorted by score (highest to lowest).
+
+    The data is stored in two arrays: 1) an array of strings named names, and 2) an array of ints named scores. These arrays are declared in the main function.   
+
+    All user input is done in the function initializeArrays. 
+    sortData function sorts both arrays in descending order by score.
+    displayData function displays the final list of names and scores.
 */
+
+
+
+
 
 
 #include <iostream>
@@ -18,119 +29,197 @@ const int SIZE = 5;
 const string PROMPT1 = "Enter the name for score #";
 const string PROMPT2 = "Enter the score for score #";
 
-void readDataEntry(string playerNames[], int playerScores[]);
 void promptUserForData(string playerNames[], int playerScores[], int &count);
-void displayDataInArray(const int list[], int size);
-
 void initializeArrays(string names[], int scores[], int size);
 void sortData(string names[], int scores[], int size);
 void displayData(const string names[], const int scores[], int size);
+int findIndexOfLargest(int list[], int startingIndex, int numItems);
 
-int findIndexOfSmallest(int list[], int startingIndex, int numItems);
+
+
+
 
 
 int main() {
-    cout << "Starting program" << endl;
-    string myFauxPlayers[SIZE] = {"Suzy", "Kim", "Bob", "Armando", "Tim"};
-    int myFauxScores[SIZE] = {600, 9900, 1012, 8000, 514};
-
-    
-    
     string names[SIZE];
     int scores[SIZE];
     
-//    initializeArrays(names, scores, SIZE);
-//    findIndexOfSmallest(myFauxScores, 0, SIZE);
-//    swap(myFauxScores[1], myFauxScores[4], SIZE);
-//    displayData(myFauxPlayers, myFauxScores, SIZE);
-    
-        sortData(myFauxPlayers, myFauxScores, SIZE);
+    initializeArrays(names, scores, SIZE);
+    sortData(names, scores, SIZE);
+    displayData(names, scores, SIZE);
 }
 
+
+
+
+
+
+/**
+ * All user input is done in the function initializeArrays. Loops and 
+ * continues to prompt users for data
+ * 
+ * @param names  string array - empty array to store player names
+ * @param scores int array - empty array to store player scores
+ * @param size   int - indicates the size of the arrays
+ */
 void initializeArrays(string names[], int scores[], int size) {
     int count = 1;
 
     promptUserForData(names, scores, count);
 
-    while(count < SIZE) {
+    while (count < SIZE) {
         count++;
         promptUserForData(names, scores, count);
-    }
-    
-//    displayData(names, scores, SIZE);
-
-//    displayDataInArray(myFauxScores);
-
-    
-    
+    }    
 }
 
+
+
+
+
+
+/**
+ * This is a helper function used by initializeArrays to prompt the user 
+    to input player names and their scores. As the program receives the data,
+    it is stored in the respective arrays
+
+ * @param playerNames  string array - empty array to store player names
+ * @param playerScores int array - empty array to store player scores
+ * @param count        int - indicates the size of the arrays
+ */
 void promptUserForData(string playerNames[], int playerScores[], int &count) {
-    cout << PROMPT1 << count << ": " << endl;
+    cout << PROMPT1 << count << ": ";
     cin >> playerNames[count - 1];
-    cout << PROMPT2 << count << ": " << endl;
+    cout << PROMPT2 << count << ": ";
     cin >> playerScores[count - 1];
 }
 
+
+
+
+
+
+
+/**
+ * Displays sorted player scores on the screen
+ * @param names  string array - sorted array of player names
+ * @param scores int array - sorted array of player scores
+ * @param size   int - indicates the size of the arrays
+ */
 void displayData(const string names[], const int scores[], int size) {
     cout << "Top Scorers: " << endl;
-    int count = size - 1;
-    while (count >= 0) {
+    int count = 0;
+    while (count < SIZE) {
         cout << names[count] << ": " << scores[count] << endl;
-        count--;
+        count++;
     }
 }
 
 
+
+
+
+
+
+/**
+ * Sorts player scores in a descending order and their respective names
+ * in the same order 
+ * 
+ * @param names  string array - unsorted array of player names
+ * @param scores int array - unsorted array of player scores
+ * @param size   int - indicates the size of the arrays
+ */
 void sortData(string names[], int scores[], int size) {
-    displayData(names, scores, size);
     int indexToSwap;
     for (int i = 0; i < size; i++) {
-        indexToSwap = findIndexOfSmallest(scores, i, size);
-        cout << "i: :" << i <<" Will move: " << indexToSwap << endl;
+        indexToSwap = findIndexOfLargest(scores, i, size);
         swap(scores[i], scores[indexToSwap]);
         swap(names[i], names[indexToSwap]);
     }
-    displayData(names, scores, size);
-
 }
 
-//void swap(string someStringArray[], idx1, idx2) {
-//
-//}
 
 
-// [0, 1, 2, 3, 4]
-//void swap(int someIntArray[], int idx1, int idx2) {
-//    int temp = someIntArray[idx1];
-//    someIntArray[idx1] = someIntArray[idx2];
-//    someIntArray[idx2] = temp;
-//}
 
 
-int findIndexOfSmallest(int list[], int startingIndex, int numItems) {
-    int indexOfSmallest = startingIndex;
-    
-//    displayDataInArray(list, numItems);
-    
+
+/**
+ * Given an array of integers, returns the index of the largest
+ * integer
+ * 
+ * @param  list          int array - indicates list of scores
+ * @param  startingIndex int - indicates the index after which to start searching
+ * @param  numItems      int - indicates the size of the array
+ * @return               int - indicates the index of the largest integer after the starting index
+ */
+int findIndexOfLargest(int list[], int startingIndex, int numItems) {
+    int indexOfLargest = startingIndex;
     
     for (int i = startingIndex + 1; i < numItems; i++) {
-//        cout << "i: " << i << " curr: " << list[i] << endl;
-        
-        if(list[i] < list[indexOfSmallest]) {
-            indexOfSmallest = i;
+        if (list[i] > list[indexOfLargest]) {
+            indexOfLargest = i;
         }
     }
-//    cout << "startingIndex: " << startingIndex << " indexOfSmallest: " << indexOfSmallest << endl;
-    return indexOfSmallest;
+
+    return indexOfLargest;
 }
 
 
-//void displayDataInArray(const int list[], int size) {
-//
-//    int count = 0;
-//    while (count < size) {
-//        cout << "item: " << list[count] << endl;
-//        count++;
-//    }
-//}
+
+
+
+
+/*
+    Enter the name for score #1:  suzy
+    Enter the score for score #1:  600
+    Enter the name for score #2:  kim
+    Enter the score for score #2:  9900
+    Enter the name for score #3:  bob
+    Enter the score for score #3:  1012
+    Enter the name for score #4:  armando
+    Enter the score for score #4:  8000
+    Enter the name for score #5:  tim
+    Enter the score for score #5:  514
+    Top Scorers: 
+    kim: 9900
+    armando: 8000
+    bob: 1012
+    suzy: 600
+    tim: 514
+
+
+
+    Enter the name for score #1:  Mike
+    Enter the score for score #1:  600
+    Enter the name for score #2:  Cathy
+    Enter the score for score #2:  200
+    Enter the name for score #3:  Tim
+    Enter the score for score #3:  300
+    Enter the name for score #4:  Liza
+    Enter the score for score #4:  200
+    Enter the name for score #5:  Felix
+    Enter the score for score #5:  -400
+    Top Scorers: 
+    Mike: 600
+    Tim: 300
+    Liza: 200
+    Cathy: 200
+    Felix: -400
+
+    Enter the name for score #1:  a
+    Enter the score for score #1:  5
+    Enter the name for score #2:  b
+    Enter the score for score #2:  3
+    Enter the name for score #3:  c
+    Enter the score for score #3:  -4
+    Enter the name for score #4:  d
+    Enter the score for score #4:  -1
+    Enter the name for score #5:  e
+    Enter the score for score #5:  2
+    Top Scorers: 
+    a: 5
+    b: 3
+    e: 2
+    d: -1
+    c: -4
+*/
