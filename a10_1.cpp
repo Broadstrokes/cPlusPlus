144075a5724cc531c5e75adf317e6905657ad6a7
Dan Sajjad <dan.sajjad@gmail.com>
2018-07-06 23:26:59 -0700

    Complete a10_1

--- a10_1.cpp
+++ a10_1.cpp
@@ -7,6 +7,53 @@
 
 
 /*
+
+    The Fraction class can be used to create objects that store a 
+    numerator & a denominator. The following functions are available:
+
+
+
+    Fraction();
+    post:   The calling object has been created and initialized so that
+            the numerator is 0, and the denominator is 1.
+
+
+    Fraction(int inNumerator, int inDenominator);
+    post:   The calling object has been created and initialized so that
+            the numerator is inNumerator, and the denominator is 
+            inDenominator.
+
+
+    void print() const;
+    post:   The calling object has been printed to the console window 
+            in the format numerator/denominator
+
+    bool isEqualTo(const Fraction otherFraction) const;
+    post:   Returns true if the calling object equals the parameter 
+            "otherDate".  Otherwise returns false.
+
+    Fraction addedTo(const Fraction otherFraction) const;
+    post:   Returns the Fraction determined by starting with the 
+            calling object and adding the otherFraction.
+
+
+    Fraction subtract(const Fraction otherFraction) const;
+    post:   Returns the Fraction determined by starting with the 
+            calling object and subtracting the otherFraction.
+
+
+    Fraction multipliedBy(const Fraction otherFraction) const;
+    post:   Returns the Fraction determined by starting with the 
+            calling object and multiplying the otherFraction.
+
+    Fraction dividedBy(const Fraction otherFraction) const;
+    post:   Returns the Fraction determined by starting with the 
+            calling object and dividing by the otherFraction.
+ 
+ */
+
+
+/*
  Create a fraction class that has various member functions
  to perform calculation on fractions
  */
@@ -43,6 +90,11 @@ Fraction::Fraction() {
     denominator = 1;
 }
 
+
+/**
+ * Sets the default values 
+ * @param 
+ */
 Fraction::Fraction(int inNumerator, int inDenominator) {
     assert(inDenominator != 0);
     
@@ -273,7 +325,19 @@ int main()
 
 
 
-
+/**
+ * This is a helper function that takes a numerator and
+ * a denominator and a starting divisor as params and outputs
+ * the reducted Fraction. The function recursively proceeds to 
+ * reduce the fraction until it can no longer be reduced and is in
+ * its simplified state
+ * 
+ * @param  num          indicated the numerator
+ * @param  denom        indicated the denominator
+ * @param  divisor      indicated the divisor that you'd like to start
+ *                      reducing the fraction by. 
+ * @return Fraction     the reduced fraction
+ */
 Fraction simplifyFraction(int num, int denom, int divisor) {
     if (divisor > num || divisor > denom) {
         Fraction output(num, denom);
@@ -302,4 +366,27 @@ Fraction simplifyFraction(int num, int denom, int divisor) {
      Program ended with exit code: 0
 */
 
+struct fractionParts {
+    int numerator;
+    int denominator;
+};
 
+
+fractionParts simplifyFraction(int numerator, int denominator, int divisor = 2);
+
+
+
+reducedFractionParts simplifyFraction(int numerator, int denominator, int divisor) {
+    if (divisor > numerator || divisor > denominator) { // Exit condition
+        reducedFractionParts output = {numerator, denominator}; 
+        return output;
+    } else {
+        if (numerator % divisor == 0 && denominator % divisor == 0) {
+            return simplifyFraction(numerator/divisor, denominator/divisor, divisor);
+        } else {
+            return simplifyFraction(numerator, denominator, divisor + 1);
+        }
+    }
+    
+    return simplifyFraction(numerator, denominator, divisor);
+}
