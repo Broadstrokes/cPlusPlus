7231e0076c48e7f4e77c8afb549ea6d58f524abe
Dan Sajjad <dan.sajjad@gmail.com>
2018-07-22 20:19:32 -0700

    Lesson 22- recursion - findSmallest (recursive try1)

--- recursion.cpp
+++ recursion.cpp
@@ -27,19 +27,31 @@ int main() {
  * @return          smallest item in the list
  */
 
+// recursive approach try 1
+int findSmallest(const int list[], int numItems) {
+	if (numItems == 1) { 	// base case;
+		return list[0];
+	} else { //If the last item in the list is smaller than the result of this function call, then we should return the last item in the list. Otherwise, we should return the result of this function call
+		if (list[numItems - 1] < findSmallest(list, numItems-1)) {
+			return list[numItems - 1];
+		} else {
+			return findSmallest(list, numItems - 1);
+		}
+	}
+}
 
 
 // iterative approach
-int findSmallest(const int list[], int numItems) {
-	int smallest = list[0], count = 0;
+// int findSmallest(const int list[], int numItems) {
+// 	int smallest = list[0], count = 0;
 
-	while (count < numItems) {
-			if (list[count] < smallest) {
-				smallest = list[count];	
-			}
-		count++;
-	}
+// 	while (count < numItems) {
+// 			if (list[count] < smallest) {
+// 				smallest = list[count];	
+// 			}
+// 		count++;
+// 	}
 
 
-	return smallest;
-}
+// 	return smallest;
+// }
