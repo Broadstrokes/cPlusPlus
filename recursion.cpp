4e56a90cdc6ce1e1df165baaf2577a32cd8ea065
Dan Sajjad <dan.sajjad@gmail.com>
2018-07-22 20:20:31 -0700

    Lesson 22 - recursion - findSmallest (recursive final)

--- recursion.cpp
+++ recursion.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -28,16 +29,35 @@ int main() {
  */
 
 // recursive approach try 1
+// int findSmallest(const int list[], int numItems) {
+// 	if (numItems == 1) { 	// base case;
+// 		return list[0];
+// 	} else { //If the last item in the list is smaller than the result of this function call, then we should return the last item in the list. Otherwise, we should return the result of this function call
+// 		if (list[numItems - 1] < findSmallest(list, numItems-1)) {
+// 			return list[numItems - 1];
+// 		} else {
+// 			return findSmallest(list, numItems - 1);
+// 		}
+// 	}
+// }
+
+
+// recursive approach - refactor try 1
+// int findSmallest(const int list[], int numItems) {
+// 	if (numItems == 1) { return list[0]; }
+// 	else {
+// 		int smallest = findSmallest(list, numItems - 1);
+// 		if (list[numItems - 1] < smallest) { return list[numItems - 1]; }
+// 		else { return smallest; }
+// 	}
+// }
+
+
+// recursive approach - refactor refactor 1
 int findSmallest(const int list[], int numItems) {
-	if (numItems == 1) { 	// base case;
-		return list[0];
-	} else { //If the last item in the list is smaller than the result of this function call, then we should return the last item in the list. Otherwise, we should return the result of this function call
-		if (list[numItems - 1] < findSmallest(list, numItems-1)) {
-			return list[numItems - 1];
-		} else {
-			return findSmallest(list, numItems - 1);
-		}
-	}
+	if (numItems == 1) return list[0];
+	else 
+		return min(list[numItems-1], findSmallest(list, numItems - 1));
 }
 
 
